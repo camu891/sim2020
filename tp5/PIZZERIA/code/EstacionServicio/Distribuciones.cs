8a6a9b49aaade3b72962cd340223b170f3324186
Nicolas Carlesso <nicolas.carlesso@silstech.com.ar>
2020-10-20 10:23:46 -0300

    Fix calculo normal, cambio que para segundo rnd no sea el opuesto si no el siguiente

--- tp5/PIZZERIA/code/EstacionServicio/Distribuciones.cs
+++ tp5/PIZZERIA/code/EstacionServicio/Distribuciones.cs
@@ -18,57 +18,6 @@ namespace Pizzeria
             return (double)a + (b - a) * rnd;
         }
 
-        public int[] GenerateCSharpRandomsPoisson(int cantidad, double lambda)
-        {
-            int[] lstRnd = new int[cantidad];
-            double a = Math.Exp(-lambda);
-            var rnd = new Random();
-            double aux = 0;
-            for (int i = 0; i < cantidad; i++)
-            {
-                double P = 1;
-                int X = -1;
-                do
-                {
-                    aux = rnd.NextDouble();
-                    P = P * aux;
-                    X++;
-                } while (P >= a);
-                lstRnd[i] = X;
-            }
-            return lstRnd;
-        }
-
-        public static double[] normalViejo(double media, double desviacion)
-        {
-            List<double[]> randoms = GenerateCSharpRandomsList(5, 10);
-            return randoms.Select(x => ((x.Sum() - 6) * desviacion) + media).ToArray();
-        }
-
-        public static List<double[]> GenerateCSharpRandomsList(int semilla, uint cantidad)
-        {
-            // Normal de convolucion
-            // Por cada 12 nros aleatorios genera 1 con dist normal
-
-            var rnd = new Random(semilla);
-            return new double[cantidad].Select(x =>
-                new double[12]
-                {
-                    rnd.NextDouble().TruncateDouble(4),
-                    rnd.NextDouble().TruncateDouble(4),
-                    rnd.NextDouble().TruncateDouble(4),
-                    rnd.NextDouble().TruncateDouble(4),
-                    rnd.NextDouble().TruncateDouble(4),
-                    rnd.NextDouble().TruncateDouble(4),
-                    rnd.NextDouble().TruncateDouble(4),
-                    rnd.NextDouble().TruncateDouble(4),
-                    rnd.NextDouble().TruncateDouble(4),
-                    rnd.NextDouble().TruncateDouble(4),
-                    rnd.NextDouble().TruncateDouble(4),
-                    rnd.NextDouble().TruncateDouble(4)
-                }
-            ).ToList();
-        }
         public static int poisson(double media, double rnd)
         {
             int seed = Convert.ToInt32(rnd * 100);
@@ -86,7 +35,9 @@ namespace Pizzeria
 
         public static double normal(double media, double desv, double rnd)
         {
-            double rnd1 = 1.0 - rnd; //uniform(0,1] random doubles
+            int seed = Convert.ToInt32(rnd * 100);
+            var rndm = new Random(seed);
+            double rnd1 = rndm.NextDouble(); //uniform(0,1] random doubles
             double randStdNormal = Math.Sqrt(-2.0 * Math.Log(rnd)) *
                          Math.Sin(2.0 * Math.PI * rnd1); //random normal(0,1)
             return media + desv * randStdNormal; //random normal(mean,stdDev^2)
